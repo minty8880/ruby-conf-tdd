1fc83344a0556aa9f0bce24163e4c24f4ff714da
Tanya Sager <tanyasager@yahoo.com.au>
2014-02-19 14:39:06 +1100

    Test 2 passing

--- roman.rb
+++ roman.rb
@@ -1,5 +1,9 @@
 def roman(n)
-  return "I"
+  if n ==1
+    return "I"
+  else 
+    return "II"
+  end
 end
 
 require "minitest/spec"
@@ -10,4 +14,7 @@ describe "roman" do
   it "converts the number 1 to the string I" do
     roman(1).must_equal "I"
   end
+  it "converts the number 2 to the string II" do
+    roman(2).must_equal "II"
+  end  
 end
