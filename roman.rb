98d24aab77ae794f7a552553e42c762973485178
Tanya Sager <tanyasager@yahoo.com.au>
2014-02-19 14:59:04 +1100

    Convert tests to use array loop

--- roman.rb
+++ roman.rb
@@ -11,19 +11,15 @@ require "minitest/autorun"
 require "minitest/pride"
 
 describe "roman" do
-  it "converts the number 1 to the string I" do
-    roman(1).must_equal "I"
-  end
-  it "converts the number 2 to the string II" do
-    roman(2).must_equal "II"
-  end  
-  it "converts the number 3 to the string III" do
-    roman(3).must_equal "III"
-  end
-  it "converts the number 4 to the string IIII" do
-    roman(4).must_equal "IIII"
-  end
-  it "converts the number 5 to the string V" do
-    roman(5).must_equal "V"
+  [
+    [1, "I"],
+    [2, "II"],
+    [3, "III"],
+    [4, "IIII"],
+    [5, "V"]
+  ].each do |row|
+    it "converts the number 1 to the string I" do
+      roman(row[0]).must_equal row[1]
+    end
   end
 end
